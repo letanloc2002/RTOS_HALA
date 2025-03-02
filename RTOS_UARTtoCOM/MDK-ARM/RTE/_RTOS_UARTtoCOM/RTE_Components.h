4ca883527d635ff37fa2a05674176601481de4bd
Loc Le Tan <letanloc.2002.work@gmail.com>
2025-03-02 16:41:55 +0700

    create project to test uart to com

---
+++ RTOS_UARTtoCOM/MDK-ARM/RTE/_RTOS_UARTtoCOM/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'RTOS_UARTtoCOM' 
+ * Target:  'RTOS_UARTtoCOM' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
