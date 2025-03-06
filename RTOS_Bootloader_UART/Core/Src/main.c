dc9141dc1e2363df9e2f680369d176dd2e6c10d1
Loc Le Tan <letanloc.2002.work@gmail.com>
2025-03-06 17:14:05 +0700

    jump to system bootloader

--- RTOS_Bootloader_UART/Core/Src/main.c
+++ RTOS_Bootloader_UART/Core/Src/main.c
@@ -1,175 +1,60 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
-#include "main.h"
+#include "stm32f4xx.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+#define BOOTLOADER_ADDRESS  0x1FFF0000  // Ð?a ch? Bootloader c?a STM32F4
+#define MAIN_FLASH_ADDRESS  0x08000000  // Ð?a ch? chuong trình chính
 
-/* USER CODE END Includes */
+#define UART USART1  // S? d?ng UART1 (PA9 = TX, PA10 = RX)
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
+// Khai báo con tr? hàm d? nh?y vào chuong trình khác
+typedef void (*JumpToApp_t)(void);
 
-/* USER CODE END PTD */
+void JumpToAddress(uint32_t address) {
+    // T?t ng?t toàn b? tru?c khi nh?y
+    __disable_irq();
 
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
+    // Set l?i MSP theo chuong trình dích
+    __set_MSP(*(volatile uint32_t*) address);
 
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
+    // L?y d?a ch? Reset_Handler() c?a chuong trình dích
+    JumpToApp_t JumpToApp = (JumpToApp_t)(*(volatile uint32_t*) (address + 4));
 
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
-
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+    // Nh?y vào chuong trình
+    JumpToApp();
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
-
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
-  }
+int UART_ReceiveCheck(void) {
+    uint32_t timeout = 1000000;  // Th?i gian ch? UART (~10 giây)
+    
+    while (timeout--) {
+        if (UART->SR & USART_SR_RXNE) {  // Ki?m tra n?u có d? li?u t? UART
+            return 1;  // Ðã nh?n du?c tín hi?u t? UART
+        }
+    }
+    return 0;  // H?t th?i gian ch? mà không có tín hi?u
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
-
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+void EnterBootloader(void) {
+    // Nh?y vào System Bootloader
+    JumpToAddress(BOOTLOADER_ADDRESS);
 }
 
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+void EnterMainFlash(void) {
+    // Nh?y vào chuong trình chính t? Flash
+    JumpToAddress(MAIN_FLASH_ADDRESS);
 }
-#endif /* USE_FULL_ASSERT */
+
+int main(void) {
+    // C?u hình UART (ch? c?n n?u không dùng CubeMX)
+    // UART_Init();
+
+    // Nh?y vào Bootloader
+    EnterBootloader();
+
+    // Ch? UART trong 10 giây
+    if (UART_ReceiveCheck() == 0) {
+        // N?u không có tín hi?u n?p, quay l?i Flash
+        EnterMainFlash();
+    }
+
+    while (1);
+}
---
+++ RTOS_Bootloader_UART/MDK-ARM/RTE/_RTOS_Bootloader_UART/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'RTOS_Bootloader_UART' 
+ * Target:  'RTOS_Bootloader_UART' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
